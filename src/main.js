d0624edf74b347b05b2d20f97ed83680aa219a04
Benjamin Van Ryseghem <benjamin.vanryseghem@gmail.com>
2016-01-20 19:46:34 +0100

    First tries of a tray icon

--- src/main.js
+++ src/main.js
@@ -1,26 +1,46 @@
-'use strict';
+"use strict";
 
-const electron = require('electron');
+const electron = require("electron");
 const app = electron.app;
+const Tray = electron.Tray;
 
-app.on('ready', init);
+let tray = null;
+
+app.on("ready", init);
 
 function init() {
-    let db = require("./db");
-    let helper = require("./helper");
-
-    let duration = helper.getDurationFor(undefined, "2016-01-19");
-    console.log(helper.formatDuration(duration));
-    //db.start();
-    //setTimeout(() => {
-    //    db.stop();
-    //}, 1000);
+    //let db = require("./db");
+    //let helper = require("./helper");
+    //
+    //let duration = helper.getDurationFor(undefined, "2016-01-19");
+    //console.log(helper.formatDuration(duration));
+    initializeTray();
 }
 
-app.on('open-url', startClockHandler);
+app.on("open-url", urlHandler);
 
-function startClockHandler(event, url) {
-    console.log('New Request');
+function urlHandler(event, url) {
+    console.log("New Request");
     console.dir(event);
     console.dir(url);
+}
+
+function initializeTray() {
+    tray = new Tray("./resources/trayIconTemplate.png");
+    tray.setHighlightMode(false);
+
+    tray.setTitle("00:00");
+    blinkingTitle(true);
+}
+
+function blinkingTitle(dotted) {
+    setTimeout(function() {
+        if (dotted) {
+            tray.setTitle("00 00");
+        } else {
+            tray.setTitle("00:00");
+        }
+
+        blinkingTitle(!dotted);
+    }, 1000);
 }
