5d34831b871972456d7a9b62a392405ee2024bd6
Benjamin Van Ryseghem <benjamin.vanryseghem@gmail.com>
2016-01-22 20:37:34 +0100

    Adds ANSI support to `cli.sh`

--- cli.sh
+++ cli.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-echo $@ | nc -U /tmp/sand-glass-sock | sed "s/^\([\"']\)\(.*\)\1\$/\2/g"
+result=`echo $@ | nc -U /tmp/sand-glass-sock | sed "s/^\([\"']\)\(.*\)\1\$/\2/g" | sed "s/u001b/033/g"`
+echo -e $result
