fbbb865b2676d3916aa074a92de3a2ced062fa99
Benjamin Van Ryseghem <benjamin.vanryseghem@gmail.com>
2016-02-13 19:11:06 +0100

    Redo application icon

--- gulpfile.js
+++ gulpfile.js
@@ -1,154 +1,156 @@
-var gulp = require('gulp');
-var sequence = require('run-sequence');
-var del = require('del');
-var merge = require('merge-stream');
-var reporters = require('jasmine-reporters');
+"use strict";
 
-var $ = require('gulp-load-plugins')({
+var gulp = require("gulp");
+var sequence = require("run-sequence");
+var del = require("del");
+var merge = require("merge-stream");
+var reporters = require("jasmine-reporters");
+
+var plugins = require("gulp-load-plugins")({
     rename: {
-        'gulp-sass-lint': 'sasslint',
-        'gulp-concat': 'concat',
-        'gulp-cssnano': 'cssnano',
-        'gulp-jshint': 'jshint',
-        'gulp-jasmine': 'jasmine',
-        'gulp-download-electron': 'electron',
-        'gulp-jasmine-browser': 'jasmineBrowser'
+        "gulp-sass-lint": "sasslint",
+        "gulp-concat": "concat",
+        "gulp-cssnano": "cssnano",
+        "gulp-jshint": "jshint",
+        "gulp-jasmine": "jasmine",
+        "gulp-download-electron": "electron",
+        "gulp-jasmine-browser": "jasmineBrowser"
     }
 });
 
 // App options
 var options = {
-    name: 'SandGlass',
-    app: 'SandGlass.app',
-    dmg: 'SandGlass-1.0.0.dmg',
-    icon: './resources/img/sandGlass.icns',
-    plist: './Info.plist',
-    bundle: 'io.sandglass.www'
+    name: "SandGlass",
+    app: "SandGlass.app",
+    dmg: "SandGlass-1.0.0.dmg",
+    icon: "./resources/img/SandGlass.icns",
+    plist: "./Info.plist",
+    bundle: "io.sandglass.www"
 };
 
-gulp.task('default', ['lint', 'tests']);
+gulp.task("default", ["lint", "tests"]);
 
-gulp.task('css', function() {
-    var scssStream = gulp.src('./resources/s+(a|c)ss/*.s+(a|c)ss')
-        .pipe($.sass())
-        .pipe($.concat('scss-files.scss'));
+gulp.task("css", function() {
+    var scssStream = gulp.src("./resources/s+(a|c)ss/*.s+(a|c)ss")
+        .pipe(plugins.sass())
+        .pipe(plugins.concat("scss-files.scss"));
 
     return merge(scssStream)
-        .pipe($.concat('sand-glass.css'))
-        .pipe($.cssnano())
-        .pipe(gulp.dest('resources/css'));
+        .pipe(plugins.concat("sand-glass.css"))
+        .pipe(plugins.cssnano())
+        .pipe(gulp.dest("resources/css"));
 });
 
-gulp.task('sass-lint', function() {
-    return gulp.src('./resources/s+(a|c)ss/**/*.s+(a|c)ss')
-        .pipe($.sasslint());
+gulp.task("sass-lint", function() {
+    return gulp.src("./resources/s+(a|c)ss/**/*.s+(a|c)ss")
+        .pipe(plugins.sasslint());
 });
 
-gulp.task('js-lint', function() {
-    return gulp.src(['./src/**/*.js', './tests/**/*.js', './resources/src/**/*.js'])
-        .pipe($.jshint())
-        .pipe($.jshint.reporter('jshint-stylish'))
-        .pipe($.jshint.reporter('fail'));
+gulp.task("js-lint", function() {
+    return gulp.src(["./src/**/*.js", "./tests/**/*.js", "./resources/src/**/*.js"])
+        .pipe(plugins.jshint())
+        .pipe(plugins.jshint.reporter("jshint-stylish"))
+        .pipe(plugins.jshint.reporter("fail"));
 });
 
-gulp.task('lint', ['sass-lint', 'js-lint']);
+gulp.task("lint", ["sass-lint", "js-lint"]);
 
 // Tests
 
-gulp.task('tests', function() {
-    return gulp.src('./tests/**/*.js')
-        .pipe($.jasmine({
+gulp.task("tests", function() {
+    return gulp.src("./tests/**/*.js")
+        .pipe(plugins.jasmine({
             reporter: new reporters.TerminalReporter()
         }));
 });
 
-gulp.task('jasmine', function() {
-    return gulp.src(['src/**/*.js', './tests/**/*.js'])
-        .pipe($.jasmineBrowser.specRunner())
-        .pipe($.jasmineBrowser.server({port: 8888}));
+gulp.task("jasmine", function() {
+    return gulp.src(["src/**/*.js", "./tests/**/*.js"])
+        .pipe(plugins.jasmineBrowser.specRunner())
+        .pipe(plugins.jasmineBrowser.server({port: 8888}));
 });
 
 // Build
 
-gulp.task('build', function() {
-    return gulp.src('').pipe($.shell([
-        'rm -rf ./release',
-        'mkdir -p <%= release %>',
+gulp.task("build", function() {
+    return gulp.src("").pipe(plugins.shell([
+        "rm -rf ./release",
+        "mkdir -p <%= release %>",
 
-        'cp -R <%= electron_app %> <%= release_app %>',
-        'mv <%= release_electron %> <%= release_example %>',
-        'mkdir -p <%= release_build %>',
+        "cp -R <%= electron_app %> <%= release_app %>",
+        "mv <%= release_electron %> <%= release_example %>",
+        "mkdir -p <%= release_build %>",
 
-        'mkdir -p <%= electron_asar %>',
-        'cp ./*.* <%= electron_asar %>/',
-        'cp -r ./src <%= electron_asar %>/src',
-        'cp -r ./resources <%= electron_asar %>/resources',
-        'mkdir -p <%= electron_asar %>/node_modules',
-        'npm install --prefix <%= electron_asar %> --production &>/dev/null',
-        'rm -rf <%= electron_asar %>/node_modules/electron-prebuilt',
-        'rm -rf <%= electron_asar %>/node_modules/.bin',
-        'asar pack <%= electron_asar %> app.asar',
-        'mv app.asar <%= release_build %>/app.asar',
-        'rm -rf <%= electron_asar %>',
+        "mkdir -p <%= electron_asar %>",
+        "cp ./*.* <%= electron_asar %>/",
+        "cp -r ./src <%= electron_asar %>/src",
+        "cp -r ./resources <%= electron_asar %>/resources",
+        "mkdir -p <%= electron_asar %>/node_modules",
+        "npm install --prefix <%= electron_asar %> --production &>/dev/null",
+        "rm -rf <%= electron_asar %>/node_modules/electron-prebuilt",
+        "rm -rf <%= electron_asar %>/node_modules/.bin",
+        "asar pack <%= electron_asar %> app.asar",
+        "mv app.asar <%= release_build %>/app.asar",
+        "rm -rf <%= electron_asar %>",
 
-        'cp <%= release_example_icon %> <%= release_electron_icon %>',
-        'cp <%= release_example_plist %> <%= release_plist %>',
+        "cp <%= release_example_icon %> <%= release_electron_icon %>",
+        "cp <%= release_example_plist %> <%= release_plist %>",
 
-        '/usr/libexec/PlistBuddy -c "Set :CFBundleVersion <%= release_version %>" <%= release_plist %>',
-        '/usr/libexec/PlistBuddy -c "Set :CFBundleDisplayName <%= release_name %>" <%= release_plist %>',
-        '/usr/libexec/PlistBuddy -c "Set :CFBundleName <%= release_name %>" <%= release_plist %>',
-        '/usr/libexec/PlistBuddy -c "Set :CFBundleIdentifier <%= release_bundle %>" <%= release_plist %>',
-        '/usr/libexec/PlistBuddy -c "Set :CFBundleExecutable <%= release_name %>" <%= release_plist %>'
+        "/usr/libexec/PlistBuddy -c \"Set :CFBundleVersion <%= release_version %>\" <%= release_plist %>",
+        "/usr/libexec/PlistBuddy -c \"Set :CFBundleDisplayName <%= release_name %>\" <%= release_plist %>",
+        "/usr/libexec/PlistBuddy -c \"Set :CFBundleName <%= release_name %>\" <%= release_plist %>",
+        "/usr/libexec/PlistBuddy -c \"Set :CFBundleIdentifier <%= release_bundle %>\" <%= release_plist %>",
+        "/usr/libexec/PlistBuddy -c \"Set :CFBundleExecutable <%= release_name %>\" <%= release_plist %>"
     ], {
         templateData: {
-            electron_app: './cache/Electron.app',
-            electron_asar: './asar',
-            release: './release/osx',
-            release_app: './release/osx/' + options.app,
-            release_build: './release/osx/' + options.app + '/Contents/Resources',
-            release_electron: './release/osx/' + options.app + '/Contents/MacOS/Electron',
-            release_example: './release/osx/' + options.app + '/Contents/MacOS/' + options.name,
-            release_bin: './release/osx/' + options.app + '"/Contents/Frameworks/Electron Helper.app/Contents/MacOS"',
+            electron_app: "./cache/Electron.app",
+            electron_asar: "./asar",
+            release: "./release/osx",
+            release_app: "./release/osx/" + options.app,
+            release_build: "./release/osx/" + options.app + "/Contents/Resources",
+            release_electron: "./release/osx/" + options.app + "/Contents/MacOS/Electron",
+            release_example: "./release/osx/" + options.app + "/Contents/MacOS/" + options.name,
+            release_bin: "./release/osx/" + options.app + "/Contents/Frameworks/Electron Helper.app/Contents/MacOS",
             release_example_icon: options.icon,
-            release_electron_icon: './release/osx/' + options.app + '/Contents/Resources/atom.icns',
-            release_plist: './release/osx/' + options.app + '/Contents/Info.plist',
+            release_electron_icon: "./release/osx/" + options.app + "/Contents/Resources/atom.icns",
+            release_plist: "./release/osx/" + options.app + "/Contents/Info.plist",
             release_example_plist: options.plist,
             release_name: options.name,
             release_bundle: options.bundle,
-            release_version: '1.0.0'
+            release_version: "1.0.0"
         }
     }));
 });
 
-gulp.task('download', function(cb) {
-    $.electron({
-        version: '0.36.5',
-        outputDir: 'cache'
+gulp.task("download", function(cb) {
+    plugins.electron({
+        version: "0.36.5",
+        outputDir: "cache"
     }, cb);
 });
 
-gulp.task('dmg', function() {
-    return gulp.src('').pipe($.shell([
-        'rm -rf ./dist',
-        'mkdir -p ./dist',
-        'node_modules/.bin/appdmg ./appdmg.json <%= release_dmg %>'
+gulp.task("dmg", function() {
+    return gulp.src("").pipe(plugins.shell([
+        "rm -rf ./dist",
+        "mkdir -p ./dist",
+        "node_modules/.bin/appdmg ./appdmg.json <%= release_dmg %>"
     ], {
         templateData: {
-            release_dmg: './dist/' + options.dmg
+            release_dmg: "./dist/" + options.dmg
         }
     }));
 });
 
-gulp.task('mac', function(cb) {
-    sequence('download', 'build', 'dmg', cb);
+gulp.task("mac", function(cb) {
+    sequence("download", "build", "dmg", cb);
 });
 
 // Clean
 
-gulp.task('clean', function(cb) {
+gulp.task("clean", function(cb) {
     del([
-        'dist',
-        'build',
-        'release'
+        "dist",
+        "build",
+        "release"
     ], cb);
 });
---
+++ logos/README.md
@@ -0,0 +1,13 @@
+# Logos
+
+<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">SandGlass</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="benjamin.vanryseghem.com" property="cc:attributionName" rel="cc:attributionURL">Benjamin Van Ryseghem</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.
+
+The files in the directory are the sources of the logo used in the app.
+
+## License
+
+Copyright :copyright: 2016 Benjamin Van Ryseghem <benjamin@vanryseghem.com>
+
+All files in this directory are distributed under a [Creative Commons
+Attribution-ShareAlike 4.0 International License](http://creativecommons.org/licenses/by-sa/4.0/). For more information, see
+  [LICENSE](./LICENSE)
